a08e4d5bc6650dbbd94736c94d6eb688a137b039
Keisuke Isobe <keisuke.isobe.2011@gmail.com>
2019-09-27 16:14:48 -0400

    multi space inputs work correctly

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ function getSearchResults(stateSearch, maxResults=10) {
   };
   let queryString = formatQueryParams(params);
   const url = baseURL + '?' + queryString;
-
   console.log(url);
-
   fetch(url)
     .then(response => {
       if (response.ok) {
@@ -32,13 +30,11 @@ function getSearchResults(stateSearch, maxResults=10) {
     });
 }
 
-
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
     $('#results').empty();
     const stateSearch = cleanStateSearch($('#js-state-search').val());
-    console.log(stateSearch);
     const maxResults = $('#js-max-results').val();
     for (let i = 0; i < stateSearch.length; i++){
       getSearchResults(stateSearch[i], maxResults);
@@ -53,28 +49,31 @@ function displayResults(responseJson) {
   console.log(responseJson);
   //$('#results-list').empty();
   // iterate through the items array
-  let htmlString = '';
   for (let i = 0; i < responseJson.data.length; i++){
     // for each video object in the items 
     //array, add a list item to the results 
     //list with the video title, description,
     //and thumbnail
     $('#results').append(`
-      <li><h3>${responseJson.data[i].fullName}</h3>
-      <p><b>URL:</b>${responseJson.data[i].url}</p>
-      <p><b>States:</b> ${responseJson.data[i].states}</p>
-      <p><b>Description:</b>${responseJson.data[i].description}</p>
+      <li><h3> ${responseJson.data[i].fullName}</h3>
+      <p><b>URL:</b> <a href=${responseJson.data[i].url}>${responseJson.data[i].url}</a></p>
+      <p><b>States:</b> ${responseJson.data[i].states.split(' ')}</p>
+      <p><b>Description:</b> ${responseJson.data[i].description}</p>
+      <p><b>Address:</b> ${responseJson.data[i].addresses[0].line1} ${responseJson.data[i].addresses[0].line2}, ${responseJson.data[i].addresses[0].line3} ${responseJson.data[i].addresses[0].city}, ${responseJson.data[i].addresses[0].stateCode} ${responseJson.data[i].addresses[0].postalCode}</p>
       </li>
     `);
   }
 }
 
-
 //functions to clean up the state searches
 function cleanStateSearch(stateSearch){
-  const stateArray = stateSearch.split(',');
+  stateSearch.replace(',', ' ');
+  const stateArray =stateSearch.split(' ');
+  for (let i = 0; i < stateSearch.length; i++){
+    stateSearch[i].trim();
+  }
+  console.log(stateArray);
   return stateArray;
 }
-
 
 $(watchForm);
